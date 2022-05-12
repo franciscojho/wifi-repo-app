e93e26ae08d8ca42472ed1721ef121bce174a2ad
Francisco Huapaya <franciscojho@hotmail.com>
2022-05-12 15:48:37 -0400

    connect signup view with backend and make it private route

--- pages/auth/signup/index.jsx
+++ pages/auth/signup/index.jsx
@@ -1,17 +1,36 @@
 import { useFormik } from "formik"
 import Box from "@mui/material/Box"
 import Button from "@mui/material/Button"
+import Cookies from "js-cookie"
+import Router from "next/router"
 
 import AppLayout from "src/components/AppLayout"
 import FormikTextField from "src/components/Inputs/FormikTextField"
 import Link from "src/components/Link"
 import MainLogo from "src/components/MainLogo"
+import noAuth from "helpers/noAuth"
 import registerformSchema from "src/schemas/registerForm"
+import supafetch from "helpers/supafetch"
+import useStore from "src/store"
 
 import AuthLayout from "../AuthLayout"
 import BackToArrow from "../BackToArrow"
 
 export default function RegisterForm() {
+    const setError = useStore((state) => state.setError)
+
+    const handleSignUp = async (values) => {
+        const resp = await supafetch.post("/api/auth/signup", values)
+        const data = await resp.json()
+        console.log(data)
+        if (data.success) {
+            Cookies.set("token", data.token)
+            Router.replace("/home")
+        } else {
+            setError(data.error)
+        }
+    }
+
     const formik = useFormik({
         initialValues: {
             name: "",
@@ -20,7 +39,7 @@ export default function RegisterForm() {
             confirmPassword: "",
         },
         validationSchema: registerformSchema,
-        onSubmit: (values) => console.log("submitted", values),
+        onSubmit: handleSignUp,
     })
 
     return (
@@ -35,7 +54,7 @@ export default function RegisterForm() {
                     sx={{
                         display: "grid",
                         gap: 2,
-                        height: "150px",
+                        height: "100%",
                         placeItems: "center",
                     }}
                 >
@@ -94,3 +113,9 @@ export default function RegisterForm() {
         </AppLayout>
     )
 }
+
+export const getServerSideProps = noAuth((context) => {
+    return {
+        props: {},
+    }
+})
