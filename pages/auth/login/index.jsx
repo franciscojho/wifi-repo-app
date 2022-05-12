e7f152dc97e97a0abeb87d83c3ecd51ddb65dc7f
Francisco Huapaya <franciscojho@hotmail.com>
2022-05-12 15:50:46 -0400

    connect login view with backend and make it private route

--- pages/auth/login/index.jsx
+++ pages/auth/login/index.jsx
@@ -1,24 +1,43 @@
 import { useFormik } from "formik"
 import Box from "@mui/material/Box"
 import Button from "@mui/material/Button"
+import Cookies from "js-cookie"
 
 import AppLayout from "src/components/AppLayout"
 import FormikTextField from "src/components/Inputs/FormikTextField"
 import Link from "src/components/Link"
 import loginFormSchema from "src/schemas/loginForm"
 import MainLogo from "src/components/MainLogo"
+import noAuth from "helpers/noAuth"
 
 import AuthLayout from "../AuthLayout"
 import BackToArrow from "../BackToArrow"
+import Router from "next/router"
+
+import supafetch from "helpers/supafetch"
+import useStore from "src/store"
 
 export default function LoginPage() {
+    const setError = useStore((state) => state.setError)
+
+    const handleLogin = async (values) => {
+        const resp = await supafetch.post("/api/auth/login", values)
+        const data = await resp.json()
+        if (data.success) {
+            Cookies.set("token", data.token)
+            Router.replace("/home")
+        } else {
+            setError(data.error)
+        }
+    }
+
     const formik = useFormik({
         initialValues: {
             email: "",
             password: "",
         },
         validationSchema: loginFormSchema,
-        onSubmit: (values) => console.log("submitted"),
+        onSubmit: handleLogin,
     })
 
     return (
@@ -74,3 +93,9 @@ export default function LoginPage() {
         </AppLayout>
     )
 }
+
+export const getServerSideProps = noAuth((context) => {
+    return {
+        props: {},
+    }
+})
