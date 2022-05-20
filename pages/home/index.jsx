4e9902941bf7864f97b48f36117a1352138929bd
Francisco Huapaya <franciscojho@hotmail.com>
2022-05-20 16:14:23 -0500

    add google maps components and menu to handle logout

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,44 +1,71 @@
-import AppLayout from "src/components/AppLayout"
-import AuthLayout from "pages/auth/AuthLayout"
+/* eslint-disable no-unused-vars */
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api"
+import { useCallback, useRef, useState } from "react"
 import Box from "@mui/material/Box"
-import Button from "@mui/material/Button"
-import Typography from "@mui/material/Typography"
 
-import Cookies from "js-cookie"
-import withAuth from "helpers/withAuth"
-import Router from "next/router"
+import { center, libraries, options } from "contants/googleMap"
+import AccountMenu from "src/components/AccountMenu"
+import AppLayout from "src/components/AppLayout"
+import GoogleMarkerList from "./GoogleMarkerList"
+import GoogleSearchModal from "./GoogleSearchModal"
+import withAuthHoc from "helpers/withAuthHoc"
 
-export default function HomePage(context) {
-    const handleSignOut = () => {
-        Cookies.remove("token")
-        Router.push("auth/login")
-    }
+function HomePage() {
+    const [tilesLoaded, setTilesLoaded] = useState(false)
+    const mapRef = useRef()
+
+    const { isLoaded, loadError } = useJsApiLoader({
+        googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+        libraries,
+    })
+
+    const onMapLoad = useCallback((map) => {
+        mapRef.current = map
+    }, [])
+
+    const panTo = useCallback(({ lat, lng }) => {
+        mapRef.current.panTo({ lat, lng })
+        mapRef.current.setZoom(14)
+    }, [])
+
+    if (loadError) return <div>Error loading maps</div>
+    if (!isLoaded) return <div>Loading maps...</div>
 
     return (
         <AppLayout>
-            <AuthLayout>
-                <Box
-                    sx={{
-                        width: "250px",
+            <Box
+                sx={{
+                    width: "100%",
+                    height: "100%",
+                    position: "relative",
+                }}
+            >
+                <AccountMenu />
+                <GoogleSearchModal
+                    panTo={panTo}
+                    sxButton={{
+                        position: "absolute",
+                        right: "40px",
+                        bottom: "40px",
+                    }}
+                />
+                <GoogleMap
+                    id="map"
+                    options={options}
+                    center={center}
+                    zoom={12}
+                    onLoad={onMapLoad}
+                    onTilesLoaded={() => setTilesLoaded(true)}
+                    mapContainerStyle={{
                         height: "100%",
-                        display: "grid",
-                        gap: 4,
+                        width: "100%",
                     }}
                 >
-                    <Typography variant="h4" textAlign="center">
-                        Welcome to the app
-                    </Typography>
-                    <Button variant="outline" onClick={handleSignOut}>
-                        Sign Out
-                    </Button>
-                </Box>
-            </AuthLayout>
+                    {tilesLoaded && <GoogleMarkerList />}
+                </GoogleMap>
+            </Box>
         </AppLayout>
     )
 }
 
-export const getServerSideProps = withAuth((context) => {
-    return {
-        props: {},
-    }
-})
+export default withAuthHoc(HomePage)
