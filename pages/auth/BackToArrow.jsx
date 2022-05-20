292930e6882d5864eab02c048f4169840358023e
Francisco Huapaya <franciscojho@hotmail.com>
2022-05-20 15:59:17 -0500

    allow component to add a back to path

--- pages/auth/BackToArrow.jsx
+++ pages/auth/BackToArrow.jsx
@@ -1,17 +1,7 @@
 import Router from "next/router"
 import ArrowBack from "@mui/icons-material/ArrowBack"
 
-export default function BackToArrow() {
-    const handleReturn = () => Router.back()
-    return (
-        <ArrowBack
-            onClick={handleReturn}
-            cursor="pointer"
-            sx={{
-                position: "absolute",
-                top: 40,
-                left: 20,
-            }}
-        />
-    )
+export default function BackToArrow({ pushTo, sx }) {
+    const handleReturn = () => (pushTo ? Router.push(pushTo) : Router.back())
+    return <ArrowBack onClick={handleReturn} cursor="pointer" sx={sx} />
 }
