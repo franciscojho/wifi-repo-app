2ccf601aa836dc78906631870e244e5abc6500c2
Francisco Huapaya <franciscojho@hotmail.com>
2022-05-22 19:26:11 -0500

    enable cloudinary env variables

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
     // reactStrictMode: true,
     env: {
         GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+        CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+        CLOUDINARY_URL: process.env.CLOUDINARY_URL,
+        CLOUDINARY_PRESET: process.env.CLOUDINARY_PRESET,
     },
 }
 
