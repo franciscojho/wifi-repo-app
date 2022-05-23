87bd4692a7a540e433f02920410fdd9e83aa1021
Francisco Huapaya <franciscojho@hotmail.com>
2022-05-22 19:31:37 -0500

    add user store slice

---
+++ src/store/slices/user.js
@@ -0,0 +1,13 @@
+const userSlice = (set, get) => ({
+    name: null,
+    email: null,
+    avatarUrl: null,
+    setUser: (user) =>
+        set({
+            name: user.name,
+            email: user.email,
+            avatarUrl: user.avatar_url,
+        }),
+})
+
+export default userSlice
