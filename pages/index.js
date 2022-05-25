482999dbf4049bcdad3dc3936f5b492f6f3aaab6
Francisco Huapaya <franciscojho@hotmail.com>
2022-05-24 21:44:23 -0500

    check authentication in main page button

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,26 @@
+import { Stack } from "@mui/material"
+import { useEffect, useState } from "react"
+import Button from "@mui/material/Button"
+import Cookies from "js-cookie"
+import Router from "next/router"
 import Typography from "@mui/material/Typography"
 
 export default function Index() {
+    const [token, setToken] = useState()
+    const handleOnClick = () => {
+        token ? Router.push("/home") : Router.push("/auth/login")
+    }
+
+    useEffect(() => {
+        setToken(Cookies.get("token"))
+    }, [])
+
     return (
-        <>
+        <Stack spacing={4}>
             <Typography variant="h1">WifiRepo</Typography>
-        </>
+            <Button onClick={() => handleOnClick()}>
+                Ir a {token ? "Home" : "Login"}
+            </Button>
+        </Stack>
     )
 }
